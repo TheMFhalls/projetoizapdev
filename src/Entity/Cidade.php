6ab05bb6eae385ae185057bdc4c37dc9d11baa2d
TheMFhalls <maiconjunioheredeslopes130696@gmail.com>
2018-04-21 23:02:54 -0300

    201804212302

--- src/Entity/Cidade.php
+++ src/Entity/Cidade.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -26,6 +28,16 @@ class Cidade
      * @ORM\JoinColumn(nullable=false)
      */
     private $estado;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Endereco", mappedBy="cidade")
+     */
+    private $enderecos;
+
+    public function __construct()
+    {
+        $this->enderecos = new ArrayCollection();
+    }
 
     public function getId()
     {
@@ -54,5 +66,41 @@ class Cidade
         $this->estado = $estado;
 
         return $this;
+    }
+
+    /**
+     * @return Collection|Endereco[]
+     */
+    public function getEnderecos(): Collection
+    {
+        return $this->enderecos;
+    }
+
+    public function addEndereco(Endereco $endereco): self
+    {
+        if (!$this->enderecos->contains($endereco)) {
+            $this->enderecos[] = $endereco;
+            $endereco->setCidade($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEndereco(Endereco $endereco): self
+    {
+        if ($this->enderecos->contains($endereco)) {
+            $this->enderecos->removeElement($endereco);
+            // set the owning side to null (unless already changed)
+            if ($endereco->getCidade() === $this) {
+                $endereco->setCidade(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return $this->nome;
     }
 }
---
+++ src/Entity/Endereco.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EnderecoRepository")
+ */
+class Endereco
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $logradouro;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $numero;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $complemento;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Cidade", inversedBy="enderecos")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $cidade;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getLogradouro(): ?string
+    {
+        return $this->logradouro;
+    }
+
+    public function setLogradouro(string $logradouro): self
+    {
+        $this->logradouro = $logradouro;
+
+        return $this;
+    }
+
+    public function getNumero(): ?int
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(int $numero): self
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    public function getComplemento(): ?string
+    {
+        return $this->complemento;
+    }
+
+    public function setComplemento(?string $complemento): self
+    {
+        $this->complemento = $complemento;
+
+        return $this;
+    }
+
+    public function getCidade(): ?Cidade
+    {
+        return $this->cidade;
+    }
+
+    public function setCidade(?Cidade $cidade): self
+    {
+        $this->cidade = $cidade;
+
+        return $this;
+    }
+}
