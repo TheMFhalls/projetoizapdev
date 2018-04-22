8cace70b5ffab537f7184c569bc6536c59012d61
TheMFhalls <maiconjunioheredeslopes130696@gmail.com>
2018-04-21 21:54:26 -0300

    201804212154

---
+++ src/Controller/EstadoController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Estado;
+use App\Form\EstadoType;
+use App\Repository\EstadoRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/estado")
+ */
+class EstadoController extends Controller
+{
+    /**
+     * @Route("/", name="estado_index", methods="GET")
+     */
+    public function index(EstadoRepository $estadoRepository): Response
+    {
+        return $this->render('estado/index.html.twig', ['estados' => $estadoRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/new", name="estado_new", methods="GET|POST")
+     */
+    public function new(Request $request): Response
+    {
+        $estado = new Estado();
+        $form = $this->createForm(EstadoType::class, $estado);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($estado);
+            $em->flush();
+
+            return $this->redirectToRoute('estado_index');
+        }
+
+        return $this->render('estado/new.html.twig', [
+            'estado' => $estado,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="estado_show", methods="GET")
+     */
+    public function show(Estado $estado): Response
+    {
+        return $this->render('estado/show.html.twig', ['estado' => $estado]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="estado_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, Estado $estado): Response
+    {
+        $form = $this->createForm(EstadoType::class, $estado);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('estado_edit', ['id' => $estado->getId()]);
+        }
+
+        return $this->render('estado/edit.html.twig', [
+            'estado' => $estado,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="estado_delete", methods="DELETE")
+     */
+    public function delete(Request $request, Estado $estado): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$estado->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($estado);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('estado_index');
+    }
+}
---
+++ src/Entity/Estado.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EstadoRepository")
+ */
+class Estado
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $nome;
+
+    /**
+     * @ORM\Column(type="string", length=2)
+     */
+    private $sigla;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getSigla(): ?string
+    {
+        return $this->sigla;
+    }
+
+    public function setSigla(string $sigla): self
+    {
+        $this->sigla = $sigla;
+
+        return $this;
+    }
+}
