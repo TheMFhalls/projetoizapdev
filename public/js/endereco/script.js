6fc3cd03bd75bac548de307e5eea003147157081
TheMFhalls <maiconjunioheredeslopes130696@gmail.com>
2018-04-22 17:09:11 -0300

    201804221709

--- public/js/endereco/script.js
+++ public/js/endereco/script.js
@@ -2,14 +2,18 @@ function find_by_cep(cep){
     $.ajax({
         url: "/endereco/find_by_cep/"+cep,
         beforeSend: function(){
-          $("form input, form textarea")
+          $("form input[type='text'], form textarea")
               .val("Carregando...");
         },
         success: function(data){
+            if(data.erro){
+                alert("Informe um CEP válido!");
+            }else{
 
+            }
         },
         error: function(xhr, status, error){
-
+            alert("Informe seu CEP");
         },
         complete: function(){
             $("input[type='text'], textarea").each(function(){
